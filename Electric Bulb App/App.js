d53f2a2f62d2a7ba7133805afe48c214381f3715
MubashirMemon <57357842+MubashirMemon@users.noreply.github.com>
2020-08-19 12:57:54 +0500

    Bulb App using JavaScript

---
+++ Electric Bulb App/App.js
@@ -0,0 +1,11 @@
+
+
+function onbulb(){
+    var bulb = document.getElementById("bulb");
+    bulb.src = "images/onbulb.jpg"
+}
+
+function offbulb(){
+    var bulb = document.getElementById("bulb");
+    bulb.src = "images/offbulb.jpg"
+}
